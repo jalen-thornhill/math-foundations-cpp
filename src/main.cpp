eb7469f5099ec2f62d3a00526e7ddf38b37b16d3
Jalen Thornhill <by.jalen.by@outlook.com>
2026-05-19 18:06:43 -0400

    made the foundations

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    int choice;
+
+     cout << "My math foundations   \n";
+     cout<< "pick your choice \n";
+
+
+     
+    do{
+        switch (choice)
+        {
+        case 0:
+            /* code */
+            break;
+        
+        default:
+            break;
+        }
+    }while(choice != 0);
+   
+
+    return 0 ;
+}
