fd8490b73ed55a83bc8403671234b200cf9d5d2c
Jalen Thornhill <by.jalen.by@outlook.com>
2026-05-20 14:37:54 -0400

    added main menu selection

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,15 @@ int main(){
     int choice;
 
      cout << "My math foundations   \n";
-     cout<< "pick your choice \n";
+    
+     
+cout << "1. Logic Truth Tables\n";
+cout << "2. Function Analyzer\n";
+cout << "3. Vector Calculator\n";
+cout << "4. Matrix Calculator\n";
+cout << "5. Numerical Calculus Tools\n";
+cout << "6. Export Session Report\n";
+cout << "0. Exit\n";
 
 
      
